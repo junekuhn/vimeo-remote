0a7aa86cf408f4d4c653f4f0b540bc5ba23f34c3
Justin Kuhn <jtkuhn3@wolftech.ad.ncsu.edu>
2019-12-18 11:47:44 -0500

    gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 #app.config.from_envvar('APP_SETTINGS')
 CORS(app,supports_credentials=True)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*", always_connect=True, ping_interval="2", ping_timeout="5", cookie=None, logger=True, engineio_logger=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
